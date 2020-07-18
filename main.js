4264fd35d6408b85d4621d537263b9bfd1722cc5
charades <raya.charades@gmail.com>
2020-07-19 00:49:08 +0900

    20-07-18 js修正

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 // 即時関数
 (function() {
-  // strictモード（お作法的に）
+  // strictモード
   'use strict'
 
-  // 要素の取得
-  // 複数になるもの前提は予めスプレッド構文を使用して配列を生成
-  const el = {
-    thumbnail: [...document.querySelectorAll('.js-thumbnail')],
-    fullsize: [...document.querySelectorAll('.js-fullsize')]
-  }
+  // 要素の取得。複数になるものは予めスプレッド構文を使用して配列を生成
+  const thumbnails = [...document.querySelectorAll('.js-thumbnail')]
 
   // クリックした要素（thumbnail）の兄弟関係にあたる要素（fullsize）に対してのDOM操作
   const visibleFullsize = e => {
@@ -19,13 +15,14 @@
 
   // fullsizeに対してのDOM操作
   const hiddenFullsize = e => {
-    const target = e
+    const target = e.target
+    if(!e.target.className.indexOf('is-visible')) return
     target.classList.add('is-hidden')
     target.classList.remove('is-visible')
   }
 
   // 取得した要素それぞれにイベントを追加
-  el.thumbnail.forEach(e => e.addEventListener('click', () => visibleFullsize(e)))
-  el.fullsize.forEach(e => e.addEventListener('click', () => hiddenFullsize(e)))
+  thumbnails.forEach(e => e.addEventListener('click', () => visibleFullsize(e)))
+  window.addEventListener('click', hiddenFullsize)
 
 })()
