1627a0999e9d106d118000588e018b53f3fbe09b
charades <raya.charades@gmail.com>
2020-07-22 21:15:23 +0900

    20-07-22 init

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 (function() {
   'use strict'
-
-  const el = {
-    thumbnail: [...document.querySelectorAll('.js-thumbnail')],
-    fullsize: [...document.querySelectorAll('.js-fullsize')]
-  }
+  
+  const thumbnail = [...document.querySelectorAll('.js-thumbnail')]
+  const fullsize = [...document.querySelectorAll('.js-fullsize')]
 
   const visibleFullsize = e => e.nextElementSibling.classList.add('is-visible')
   const hiddenFullsize = e => e.target.classList.remove('is-visible')
 
-  el.thumbnail.forEach(e => e.addEventListener('click', () => visibleFullsize(e)))
-  el.fullsize.forEach(e => e.addEventListener('click', hiddenFullsize))
+  thumbnail.forEach(e => e.addEventListener('click', () => visibleFullsize(e)))
+  fullsize.forEach(e => e.addEventListener('click', hiddenFullsize))
 
 })()
